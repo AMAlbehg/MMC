29afb9e51df9aebecc3ce821dbad5092cc9d19a9
AMAlbehg <benmoussa17amal@gmail.com>
2024-01-26 10:26:43 +0100

    add mapper

--- MMC/MMC.Application/Mapping/AutoMapperProfile.cs
+++ MMC/MMC.Application/Mapping/AutoMapperProfile.cs
@@ -1,16 +1,19 @@
 ﻿using AutoMapper;
+using MMC.Application.Modules.Speakers.Commands.Create;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MMC.Application.Mapping
 {
     public class AutoMapperProfile : Profile
     {
-        public AutoMapperProfile() { 
-          
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateSpeakerCommand, DOMAIN.Models.Speaker>().ReverseMap();
         }
     }
 }
